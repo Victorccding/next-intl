01185050d8c27d1f788c1709cdf23be16d632de0
Victor Ding <weikding@amazon.com>
2023-07-23 21:46:49 -0700

    test

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,3 +1,10 @@
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({
+  locales: ['en', 'de'],
+  defaultLocale: 'en'
+});
+
 export const config = {
   // Skip all paths that should not be internationalized
   matcher: ['/((?!api|_next|.*\\..*).*)']
